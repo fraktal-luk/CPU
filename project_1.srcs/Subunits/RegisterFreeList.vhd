5486d878c4d775e1afae10e92359d7b2b7db09d8
fraktal-luk <fraktal3725@gmail.com>
2019-05-29 00:40:42 +0200

    Developing optimized FreeList...

--- project_1.srcs/Subunits/RegisterFreeList.vhd
+++ project_1.srcs/Subunits/RegisterFreeList.vhd
@@ -185,4 +185,120 @@ begin
 		
 		newPhysDests <= newPhysDestsAsync; -- CMP: Async => Sync
 		newPhysDestPointer <= freeListTakeNumTags(0); -- BL_OUT	
+		
+		
+		IMPL_2: block
+            signal listContent: PhysNameArray(0 to FREE_LIST_SIZE-1) := initList;
+            signal listPtrTake: SmallNumber := i2slv(0, SMALL_NUMBER_SIZE);
+            signal listPtrPut: SmallNumber := i2slv(N_PHYS - 32 - FP_1, SMALL_NUMBER_SIZE);
+            
+            signal listFront, listBack: PhysNameArray(0 to 7) := (others => (others => '0'));
+            signal physPtrTake: SmallNumber := i2slv(0, SMALL_NUMBER_SIZE);
+            signal physPtrPut: SmallNumber := i2slv(N_PHYS - 32 - FP_1, SMALL_NUMBER_SIZE);
+      
+            signal numFront, numBack: integer := 0;
+        begin
+            
+            --freeListTakeNumTags(0) <= i2slv((slv2u(listPtrTake)) mod FREE_LIST_SIZE, SMALL_NUMBER_SIZE);
+
+
+            -- 
+            --     0 1 2 3
+            --  [ (x x x x) (x x x x) ]
+            --
+            --
+            --
+
+            READ_DESTS: for i in 0 to WIDTH-1 generate
+                --newPhysDestsAsync(i) <= listContent((slv2u(listPtrTake) + i) mod FREE_LIST_SIZE);
+            end generate;
+
+
+            SYNCHRONOUS: process(clk)
+                variable indPut, indTake: integer := 0;
+                variable nTaken, nPut, numFrontVar, numBackVar: integer := 0;
+                variable indPutV, indTakeV, physPtrTakeVar, physPtrPutVar: SmallNumber := i2slv(0, SMALL_NUMBER_SIZE);
+                
+                variable listFrontExt, listBackExt: PhysNameArray(0 to 11) := (others => (others => '0'));
+                variable listFrontExtM4: PhysNameArray(0 to 15) := (others => (others => '0'));                
+            begin
+                if rising_edge(clk) then
+                        indTake := slv2u(listPtrTake); 
+                        indPut := slv2u(listPtrPut);                            
+                                        
+                        nTaken := countOnes(freeListTakeSel);
+                        nPut := countOnes(freeListPutSel);
+                     
+                        numFrontVar := numFront;
+                        listFrontExt(0 to 7) := listFront;
+                        
+                        if freeListRewind = '1' then
+                            listPtrTake <= freeListWriteTag; -- Indexing TMP                            
+                            physPtrTake(SMALL_NUMBER_SIZE-1 downto 2) <= freeListWriteTag(SMALL_NUMBER_SIZE-1 downto 2);                         
+                            numFrontVar := -slv2u(freeListWriteTag(1 downto 0));
+                        end if;
+                        
+                        if freeListTakeAllow = '1' and freeListRewind = '0' then
+                            for i in 0 to WIDTH-1 loop
+                                --newPhysDestsSync(i) <= listContent((slv2u(listPtrTake) + i) mod FREE_LIST_SIZE);
+                            end loop;
+                            indTake := (indTake + nTaken) mod FREE_LIST_SIZE; -- CMP: nTaken => WIDTH
+                            indTakeV := i2slv(indTake, listPtrTake'length);
+                            
+                            numFrontVar := numFrontVar - nTaken;                          
+                            listFrontExt(0 to 7) := listFrontExt(0 + nTaken to 7 + nTaken);                          
+                            listPtrTake <= indTakeV;                            
+                        end if;
+                        
+                        if freeListRewind = '0' then
+                             if numFrontVar <= 4 then
+                                
+                                listFrontExtM4(4 to 15) := listFrontExt;                                
+                                listFrontExtM4(numFrontVar + 4 to numFrontVar + 3 + 4) := listContent(slv2u(physPtrTake) to slv2u(physPtrTake) + 3);
+                                listFrontExt := listFrontExtM4(4 to 15);                                   
+                                numFrontVar := numFrontVar + 4;
+                                physPtrTake <= i2slv(slv2u(physPtrTake) + 4, SMALL_NUMBER_SIZE);
+                             end if;
+                             
+                             listFront <= listFrontExt(0 to 7);
+                        end if;
+                        
+                        numFront <= numFrontVar;
+                        
+                        
+                        listBackExt(0 to 7) := listBack;
+                        numBackVar := numBack;
+                        if freeListPutAllow = '1' then
+                            for i in 0 to WIDTH-1 loop
+                                -- for each element of input vec
+                                if freeListPutSel(i) = '1' then
+                                    --listContent(indPut) <= physCommitFreedDelayed(i);
+                                    indPut := (indPut + 1) mod FREE_LIST_SIZE;
+                                        assert isNonzero(physCommitFreedDelayed(i)) = '1' report "Putting 0 to free list!";
+                                end if;    
+                            end loop;
+                            listPtrPut <= i2slv(indPut, listPtrPut'length);
+                            
+                            listBackExt(numBackVar to numBackVar + 3) := physCommitFreedDelayed;
+                            numBackVar := numBackVar + nPut;
+                            if numBackVar >= 4 then
+                                for j in 0 to 3 loop
+                                    listContent((slv2u(physPtrPut) + j) mod FREE_LIST_SIZE) <= listBackExt(j);
+                                end loop;
+                            
+                                listBackExt(0 to 7) := listBackExt(4 to 11);
+                                numBackVar := numBackVar - 4;
+                                
+                                physPtrPut <= i2slv(slv2u(physPtrPut) + 4, SMALL_NUMBER_SIZE);                          
+                            end if;                                                           
+                        end if;                        
+                        
+                        listBack <= listBackExt(0 to 7);
+                        numBack <= numBackVar;
+                        
+                end if;
+            end process;            
+        
+        end block;		
+		
 end Behavioral;
