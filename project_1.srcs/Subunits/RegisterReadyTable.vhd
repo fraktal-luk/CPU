60d8c1b5612b9f0b85c5cd13d181c6836595012a
fraktal-luk <fraktal3725@gmail.com>
2020-02-10 23:57:00 +0100

    Area reduced in ready table

--- project_1.srcs/Subunits/RegisterReadyTable.vhd
+++ project_1.srcs/Subunits/RegisterReadyTable.vhd
@@ -71,7 +71,128 @@ begin
     return res;
 end function;
 
+
+    subtype MatchVec is std_logic_vector(0 to 15);
+    type MatchMat is array(0 to 3) of matchVec;
+    type MaskLH is array(0 to 1) of std_logic_vector(0 to N_PHYSICAL_REGS-1);
+   
+
+function matchInputL(adr: PhysName; en0, en1: std_logic) return MatchVec is
+        variable matchL, matchH: MatchVec := (others=> '0');
+    begin
+    
+        -- Cut input addresses to 2x4 bit parts
+        for i in 0 to 15 loop            
+            --for j in 0 to 3 loop
+                matchL(i) := bool2std((adr(3 downto 0) & en0 & en1) = (i2slv(i, 4) & "11"));
+                matchH(i) := bool2std(adr(7 downto 4) = i2slv(i, 4));                
+            --end loop;
+        end loop;
+    
+    
+    return matchL;
+end function; 
+
+function matchInputH(adr: PhysName) return MatchVec is
+    variable matchL, matchH: MatchVec := (others=> '0');
 begin
+
+    -- Cut input addresses to 2x4 bit parts
+    for i in 0 to 15 loop            
+        --for j in 0 to 3 loop
+            matchL(i) := bool2std(adr(3 downto 0) = i2slv(i, 4));
+            matchH(i) := bool2std(adr(7 downto 4) = i2slv(i, 4));                
+        --end loop;
+    end loop;
+    
+    
+    return matchH;
+end function;
+
+
+function getSelectionMaskL(matchL, matchH: MatchVec) return std_logic_vector is
+    variable res: std_logic_vector(0 to N_PHYSICAL_REGS-1) := (others => '0');
+    variable ind: natural := 0;
+begin
+    for ih in 0 to 15 loop
+        for il in 0 to 15 loop
+            ind := 16*ih + il;
+            if ind >= N_PHYSICAL_REGS then
+                next;
+            end if;
+            res(ind) := matchL(il);
+            
+        end loop;
+    end loop;
+    
+    return res;
+end function;
+
+function getSelectionMaskH(matchL, matchH: MatchVec) return std_logic_vector is
+    variable res: std_logic_vector(0 to N_PHYSICAL_REGS-1) := (others => '0');
+    variable ind: natural := 0;
+begin
+    for ih in 0 to 15 loop
+        for il in 0 to 15 loop
+            ind := 16*ih + il;
+            if ind >= N_PHYSICAL_REGS then
+                next;
+            end if;
+            res(ind) := matchH(ih);
+            
+        end loop;
+    end loop;
+    
+    return res;
+end function;
+
+
+function getSelMask2(adr: PhysName; en0, en1: std_logic) return MaskLH is
+    variable res: MaskLH := (others => (others => '0'));
+    variable matchL, matchH: MatchVec := (others => '0');
+begin
+    matchL := matchInputL(adr, en0, en1);
+    matchH := matchInputH(adr);
+    res(0) := getSelectionMaskL(matchL, matchH);
+    res(1) := getSelectionMaskH(matchL, matchH);
+    return res;
+end function;
+
+
+
+signal matchL, matchH: MatchVec := (others => '0');
+signal selMask, selMask0, selMask1, selMask2, selMask3, selMaskS: MaskLH := (others => (others => '0'));
+
+function nextReadyMask(sm0, sm1, sm2, sm3, sms: MaskLH; state: std_logic_vector) return std_logic_vector is
+    variable res: std_logic_vector(0 to N_PHYSICAL_REGS-1) := (others => '0');
+    variable tmp, tmp2, tmp3: std_logic := '0';
+begin
+    for i in 0 to N_PHYSICAL_REGS-1 loop
+        tmp :=  (sm0(0)(i) and sm0(1)(i))
+            or  (sm1(0)(i) and sm1(1)(i))
+            or  (sm2(0)(i) and sm2(1)(i));
+        
+        tmp2 := tmp or (sm3(0)(i) and sm3(1)(i));
+        tmp3 := sms(0)(i) and sms(1)(i);
+        
+        res(i) := tmp3 or (state(i) and not tmp2);
+        
+    end loop;
+
+    return res;
+end function;
+
+begin
+
+--    matchL <= matchInputL(newPhysDests(0));
+--    matchH <= matchInputH(newPhysDests(0));
+    selMask0 <= getSelMask2(newPhysDests(0), readyTableClearSel(0), readyTableClearAllow);
+    selMask1 <= getSelMask2(newPhysDests(1), readyTableClearSel(1), readyTableClearAllow);
+    selMask2 <= getSelMask2(newPhysDests(2), readyTableClearSel(2), readyTableClearAllow);
+    selMask3 <= getSelMask2(newPhysDests(3), readyTableClearSel(3), readyTableClearAllow);
+    selMaskS <= getSelMask2(altDests(0), altMask(0), '1');
+
+
 		readyTableClearAllow <= sendingToReserve; -- for ready table
 		readyTableClearSel <= (others => '1'); -- No need to prevent free yet-unallocated regs from clearing!	
 		
@@ -80,8 +201,14 @@ begin
 		
 		      --newPhysDests <= getPhysicalDests(stageDataReserved);
 		
+
+
+        
+
+		
+		
 		IMPL: block
-			signal content: std_logic_vector(0 to N_PHYSICAL_REGS-1) := initList(IS_FP);--(0 to 31 => '1', others => '0');
+			signal content, content2: std_logic_vector(0 to N_PHYSICAL_REGS-1) := initList(IS_FP);--(0 to 31 => '1', others => '0');
 		begin
 				SYNCHRONOUS: process(clk)
 				begin
@@ -100,10 +227,13 @@ begin
                                 -- set 
                                 content(slv2u(altDests(i))) <= '1';
                             end if;
-                        end loop;								
+                        end loop;
+                        
+                            content2 <= nextReadyMask(selMask0, selMask1, selMask2, selMask3, selMaskS, content2);
+                            content2(0) <= '1'; 								
 					end if;
 				end process;
 				
-			readyRegFlagsNext <= extractReadyRegBits(content, stageDataReserved);				
+			readyRegFlagsNext <= extractReadyRegBits(content2, stageDataReserved);				
 		end block;
 end Behavioral;
