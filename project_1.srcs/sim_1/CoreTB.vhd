554ee6ba233290b975de7f119c4f864b39d726f6
fraktal-luk <fraktal3725@gmail.com>
2020-10-15 21:37:40 +0200

    Merged emulation into CoreTB (optional)

--- project_1.srcs/sim_1/CoreTB.vhd
+++ project_1.srcs/sim_1/CoreTB.vhd
@@ -36,7 +36,10 @@ ENTITY CoreTB IS
 END CoreTB;
  
 ARCHITECTURE Behavior OF CoreTB IS
-
+    
+    constant EMULATION: boolean := false;
+    constant CORE_SIMULATION: boolean := true;
+    
     -- Component Declaration for the Unit Under Test (UUT)
 
     COMPONENT Core -- FrontPipe0
@@ -101,13 +104,14 @@ ARCHITECTURE Behavior OF CoreTB IS
 	signal memReadValue, memReadValuePrev, memWriteAddress, memWriteValue: Mword := (others => '0');
 
 	signal dataMem: WordArray(0 to 255) := (
-					72 => X"00000064",
-					250 => X"00000055",
 					others => (others => '0'));
-
+    
+    signal resetDataMem: std_logic := '0';
+    
     -- Clock period definitions
     constant clk_period : time := 10 ns;
 	--signal memEn: std_logic := '0';
+
  
 	signal prog: ProgramBuffer;
 	signal machineCode: WordArray(0 to prog'length-1);
@@ -116,8 +120,28 @@ ARCHITECTURE Behavior OF CoreTB IS
     signal testToDo, testDone, testFail: std_logic := '0';
     
     signal currentTest, currentSuite: string(1 to 20);
+
+    constant TIME_STEP: time := 1 ns;
+    
+    signal programMemory: WordArray(0 to 1023);
+    signal dataMemory: ByteArray(0 to 4095);
+    
+    signal instructionWord: Mword;
+    signal disasm: string(1 to 51);
+    
+    
+    signal cpuState: CoreState := INIT_CORE_STATE;
+    
+    
+        signal internalOp: InternalOperation;
+        signal opFlags: std_logic_vector(0 to 2);
+        
+    signal okFlag, errorFlag: std_logic := '0';
     
 BEGIN
+
+    okFlag <= bool2std(opFlags = "001");
+    errorFlag <= bool2std(opFlags = "100");
 
    -- Instantiate the Unit Under Test (UUT)
    uut: Core PORT MAP (
@@ -172,6 +196,9 @@ BEGIN
        variable testName, suiteName: line;
        file suiteFile: text open read_mode is "suite_names.txt";
        file testFile: text;
+	   
+        variable insWordVar: Word;
+        variable intOpVar: InternalOperation;	   
    begin
 	
 	  wait for 110 ns;
@@ -205,39 +232,76 @@ BEGIN
                     currentTest <= (others => ' ');
                     currentSuite(1 to suiteName.all'length) <= suiteName.all;
                     currentTest(1 to testName.all'length) <= testName.all;
+
+                        instructionWord <= (others => 'U');
+                        internalOp <= DEFAULT_INTERNAL_OP;
+                        disasm <= (others => ' ');
+						opFlags <= (others => '0');
+						cpuState <= INIT_CORE_STATE;
+						dataMemory <= (others => (others => '0'));
+               
+                resetDataMem <= '1';
                
               wait until rising_edge(clk);
+                
+                resetDataMem <= '0';
+                
+              --  dataMem <= (others => (others => '0'));
               
               testProgram(0 to machineCode'length-1) <= machineCode(0 to machineCode'length-1);
               testProgram(512/4) <= ins6L(j, -512);-- TEMP! 
               testProgram(384/4) <= ins655655(ext2, 0, 0, send, 0, 0);
               testProgram(384/4 + 1) <= ins6L(j, 0); -- idle loop          
 
-        
-              testToDo <= '1';
-              int0b <= '1';
-              wait until rising_edge(clk);
-              testToDo <= '0';
-              int0b <= '0';
-    
+                programMemory(0 to machineCode'length-1) <= machineCode(0 to machineCode'length-1);
+                programMemory(512/4) <= ins6L(j, -512);-- TEMP! 
+                programMemory(384/4) <= ins655655(ext2, 0, 0, send, 0, 0);
+                programMemory(384/4 + 1) <= ins6L(j, 0); -- idle loop   
+
+                if CORE_SIMULATION then
+                      testToDo <= '1';
+                      int0b <= '1';
+                      wait until rising_edge(clk);
+                      testToDo <= '0';
+                      int0b <= '0';
+                end if;
+            
               disasmToFile(testName.all & "_disasm.txt", testProgram);
                     
               report "Waiting for completion...";
-    
-              loop
-                  wait until rising_edge(clk);
 
-                  if testDone = '1' then
-                      report "Test done";
-                      exit;
-                  end if;
-                  
-                  if testFail = '1' then
-                      report "TEST FAIL: " & testName.all;
-                      
-                      wait;                     
-                  end if;                  
-              end loop;
+                
+                if EMULATION then    
+                    -- Now doing the actual test 
+                    while opFlags /= "100" and opFlags /= "001" loop -- ERROR or SEND (completed)
+                        insWordVar := programMemory(slv2u(cpuState.nextIP)/4);
+                        instructionWord <= insWordVar;
+                        intOpVar := decode(cpuState.nextIP, insWordVar);
+                        internalOp <= intOpVar;
+                        disasm <= disasmWithAddress(slv2u(cpuState.nextIP), programMemory(slv2u(cpuState.nextIP)/4));
+                        
+                        performOp(cpuState, dataMemory, intOpVar, opFlags);
+                                          
+                        wait for TIME_STEP;
+                    end loop;
+                end if;
+
+                if CORE_SIMULATION then
+                      loop
+                          wait until rising_edge(clk);
+        
+                          if testDone = '1' then
+                              report "Test done";
+                              exit;
+                          end if;
+                          
+                          if testFail = '1' then
+                              report "TEST FAIL: " & testName.all;
+                              
+                              wait;                     
+                          end if;                  
+                      end loop;
+                end if;
                 
               wait until rising_edge(clk);
           end loop;
@@ -400,7 +464,9 @@ BEGIN
 	DATA_MEM: process (clk)
 	begin
 		if rising_edge(clk) then
-			if en = '1' then			
+		    if resetDataMem = '1' then
+		        dataMem <= (others => (others => '0'));
+			elsif en = '1' then			
 				-- TODO: define effective address exact size
 			
 				-- Reading
