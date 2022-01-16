d9fec5ebd43fc92d7d72d120953b04931b3cdf54
fraktal-luk <fraktal3725@gmail.com>
2022-01-16 20:13:11 +0100

    Added nFull to IQ

--- project_1.srcs/Subunits/IssueQueue.vhd
+++ project_1.srcs/Subunits/IssueQueue.vhd
@@ -26,7 +26,8 @@ entity IssueQueue is
 		DONT_MATCH1: boolean := false;
 		FORWARDING: ForwardingModeArray := (0 => (-100, false));  -- Can be used immediately
 		FORWARDING1: ForwardingModeArray := (0 => (-100, false));
-		FORWARDING_D: ForwardingModeArray := (0 => (-100, false)) -- Can be used with 1 cycle delay
+		FORWARDING_D: ForwardingModeArray := (0 => (-100, false)); -- Can be used with 1 cycle delay
+		NONSHIFT: boolean := false
 	);
 	port(
 		clk: in std_logic;
@@ -62,6 +63,9 @@ architecture Behavioral of IssueQueue is
 
     signal queueContent: SchedulerInfoArray(0 to QUEUE_SIZE_EXT-1) := (others => DEFAULT_SCHEDULER_INFO);
         
+        
+        signal	   nFull_C,    nFull, nFullNext, nIn, nOut, recoveryCounter: SmallNumber := (others => '0');
+
         type slv2D is array(natural range <>, natural range <>) of std_logic;
         signal TMP_ageMatrix, TMP_ageMatrixNext, selMatrix: slv2D(0 to QUEUE_SIZE_EXT-1, 0 to QUEUE_SIZE_EXT-1) := (others => (others => '0'));
         signal TMP_insertionLocs: slv2D(0 to QUEUE_SIZE_EXT-1, 0 to PIPE_WIDTH-1) := (others => (others => '0'));
@@ -167,6 +171,7 @@ architecture Behavioral of IssueQueue is
     signal fullMask: std_logic_vector(0 to QUEUE_SIZE_EXT-1) := (others => '0');
 
 	signal anyReadyFull, anyReadyLive, sends, sendingKilled, isSent, isSent2, sentKilled, isEmpty: std_logic := '0';
+    	signal anyReadyFull_NS, anyReadyLive_NS, sends_NS, sendingKilled_NS, isSent_NS, isSent2_NS, sentKilled_NS, isEmpty_NS: std_logic := '0';
     
     signal selectedSlot: SchedulerInfo := DEFAULT_SCHEDULER_INFO;
 	signal dispatchDataNew_T: SchedulerEntrySlot := DEFAULT_SCH_ENTRY_SLOT;
@@ -176,6 +181,8 @@ architecture Behavioral of IssueQueue is
 
     signal S_firstFree: natural := 0;       
     signal S_selectedStatic, S_selectedStaticRestored: StaticInfo := DEFAULT_STATIC_INFO;
+                
+        signal isFull, isAlmostFull: std_logic := '0';
                                   
         signal ch0, ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8: std_logic := '0';
 
@@ -242,6 +249,16 @@ begin
     newArr_T <= assignStaticPtr(newArr, S_firstFree);
     
         ch0 <= bool2std(selMask_TrNS = selMask_NS);
+        ch1 <= bool2std(nFull = nFull_C);
+        ch2 <= bool2std(dispatchDataNew_NS = dispatchDataNew_T);
+    
+        nFull_C <= i2slv(countOnes(fullMask_NS), SMALL_NUMBER_SIZE);
+    
+        nFullNext <=        sub(i2slv(countOnes(fullMask_NS), SMALL_NUMBER_SIZE), nOut) when slv2u(recoveryCounter) = 1
+                      else  add(nFull, sub(nIn, nOut));
+
+        nIn <= i2slv(countOnes(extractFullMask(newArr_T)), SMALL_NUMBER_SIZE) when prevSendingOK = '1' else (others => '0'); 
+        nOut <= i2slv(1, SMALL_NUMBER_SIZE) when (isSent and not sentKilled) = '1' else (others => '0');
     
     MANAGEMENT: block
         signal trialMask, readyMaskLive, killMask, readyMaskAll, selMask: std_logic_vector(0 to QUEUE_SIZE_EXT-1) := (others => '0');
@@ -260,7 +277,24 @@ begin
                 sentKilled <= sendingKilled;            
                 isSent <= sends;
                 isSent2 <= isSent;
-    
+                    
+                    sentKilled_NS <= sendingKilled_NS;            
+                    isSent_NS <= sends_NS;
+                    isSent2_NS <= isSent_NS;
+                
+                nFull <= nFullNext;
+                    
+                if events.lateEvent = '1' or events.execEvent = '1' then
+                    recoveryCounter <= i2slv(1, SMALL_NUMBER_SIZE);
+                elsif isNonzero(recoveryCounter) = '1' then
+                    recoveryCounter <= addInt(recoveryCounter, -1);
+                end if;
+                
+                recoveryCounter(7 downto 1) <= (others => '0'); -- Only 1 bit needed here    
+           
+                isFull <= cmpGtU(nFullNext, QUEUE_SIZE_EXT - PIPE_WIDTH);
+                isAlmostFull <= cmpGtU(nFullNext, QUEUE_SIZE_EXT - 2*PIPE_WIDTH);
+                                    
             end if;
         end process;
 
@@ -293,6 +327,13 @@ begin
             readyMaskAll_NS <= getReadyVec(controlSigs_NS);
             readyMaskLive_NS <= getReadyLiveVec(controlSigs_NS);
             
+            -- Scalar signals
+            anyReadyLive_NS <= isNonzero(readyMaskLive_NS);
+            anyReadyFull_NS <= isNonzero(readyMaskAll_NS);
+            sends_NS <= anyReadyFull_NS and nextAccepting;
+            sendingKilled_NS <= isNonzero(killMask_NS and selMask_NS);          
+            
+            
             -- TMP: find by tag where the op is corresponding to selection from shifting queue      
             selMask_TrNS <= --getSelectedVec(controlSigs_NS);
                             TMP_trSelMask(selMask, queueContent, queueContent_NS);
@@ -301,6 +342,7 @@ begin
             selMatrix <= TMP_getSelMatrix(readyMaskAll_NS, TMP_ageMatrix);
             
         isEmpty <= not isNonzero(fullMask);
+            isEmpty_NS <= not isNonzero(fullMask_NS);
     
         -- Content manipulation
         queueContentRR <= updateRenameIndex(queueContent);
@@ -322,23 +364,42 @@ begin
             queueContentNext_NS <= iqNext_NS(queueContentUpdated_NS, newArr_T, prevSendingOK, sends, killMask_NS, trialMask_NS, selMask_NS, readyRegFlags, 0);
     
         selectedSlot <= prioSelect16(queueContentUpdatedSel, readyMaskAll);
+            selectedSlot_NS <= prioSelect16(queueContentUpdatedSel_NS, selMask_NS);
     
     end block;
-    
+
     dispatchDataNew_T <= getSchedEntrySlot(selectedSlot);
-
-    -- Output signals
-	schedulerOut <= TMP_restoreState(sends, dispatchDataNew_T.ins, dispatchDataNew_T.state);
-
-	acceptingOut <= not isNonzero(fullMask(4 to 7));
-	acceptingMore <= not isNonzero(fullMask(0 to 7));
-
-    outputSignals <=   (sending => sends,
-                        cancelled => sentKilled,
-                        ready => anyReadyLive,
-                        empty => isEmpty);
+        dispatchDataNew_NS <= getSchedEntrySlot(selectedSlot_NS);
 
 
+    WHEN_SH: if not NONSHIFT generate
+        -- Output signals
+        schedulerOut <= TMP_restoreState(sends, dispatchDataNew_T.ins, dispatchDataNew_T.state);
+
+        acceptingOut <= not isNonzero(fullMask(4 to 7));
+        acceptingMore <= not isNonzero(fullMask(0 to 7));
+    
+        outputSignals <=   (sending => sends,
+                            cancelled => sentKilled,
+                            ready => anyReadyLive,
+                            empty => isEmpty);    
+    end generate;
+
+    WHEN_NSH: if NONSHIFT generate
+        -- Output signals
+        schedulerOut <= TMP_restoreState(sends_NS, dispatchDataNew_NS.ins, dispatchDataNew_NS.state);
+
+        acceptingOut <= not isFull;
+        acceptingMore <= not isAlmostFull;
+    
+        outputSignals <=   (sending => sends_NS,
+                            cancelled => sentKilled_NS,
+                            ready => anyReadyLive_NS,
+                            empty => isEmpty_NS);
+    end generate;
+
+            ch3 <= bool2std(sends = sends_NS and sentKilled = sentKilled_NS and anyReadyLive = anyReadyLive_NS and isEmpty = isEmpty_NS);
+            --ch4 <= bool2std();
     
     STATIC_PART_CONTROL: block
         type StaticMaskArray is array(0 to STATIC_ARRAY_SIZE-1) of std_logic_vector(0 to PIPE_WIDTH-1);
