c56721b5c01d7a4123132880e8ee7487a247f9a8
fraktal-luk <fraktal3725@gmail.com>
2023-05-09 20:51:39 +0200

    Cont...

--- project_1.srcs/Subunits/MultiplierDivider.vhd
+++ project_1.srcs/Subunits/MultiplierDivider.vhd
@@ -41,11 +41,20 @@ end MultiplierDivider;
 
 architecture Behavioral of MultiplierDivider is
     signal dataToMul, dataMulE0, dataMulE1, dataMulE2, divSlot: ExecResult := DEFAULT_EXEC_RESULT;
-    signal trialled, kill, divUnlock, divPrepareSend, divResultSending, divResultSent, divResultSent2, divReady, remReady: std_logic := '0';
-    signal mulResult, quot00, quot10, quot01, quot11, rem00, rem10, rem01, rem11: Word := (others => '0');
+    signal divAllowed, sendingDivIssued, divRR, divFull,
+            divUnlock, 
+            divPrepareSend, divResultSending,
+            divResultSent, divResultSent2, divReady, remReady, sendingDivRR: std_logic := '0';
+
+    signal sg0, sg1, isLowE0, isLowE1: std_logic := '0';
+    signal arg0, arg1, mulResult, divRes: Word := (others => '0');
+    signal resLongE1: Dword := (others => '0');
+    
+    signal divQuot, divRem: Word := (others => '0');
 begin
 
-    kill <= (trialled and events.execEvent) or events.lateEvent;
+    sendingDivIssued <= preInput.full and usesDivider(preInput); -- Speculative because it doesn't take into account kill signals?
+    sendingDivRR <= prevSending and usesDivider(input);
 
     -- This must mux issued multiply with div result
     dataToMul <= divSlot when divResultSending = '1'
@@ -64,96 +73,61 @@ begin
     end process;
 
 
-    EEEEEE: block
-        signal divAllowed, sendingDivIssued, sendingDivRR, divRR, divFull, usingDiv, usingRem, isUnsigned,
-               new00, new10, new01, new11, sgA, sgB, isLow0, isLow1, signSel0, signSel1: std_logic := '0';
-        signal divTime: SmallNumber := sn(0);
-        signal resLong1, divisorS, sum00, sum10, sum01, sum11, diff00, diff10, diff01, diff11: Dword := (others => '0');
-        signal result00, result10, result01, result11, divRes, divQuot, divRem, arg0, arg1: Word := (others => '0');
+
+    MAIN: process (clk)
     begin
+        if rising_edge(clk) then
+            -- stage E0
+            isLowE0 <= bool2std(input.st.operation.arith = opMul);
 
-           divQuot <= quot11 when (signSel0 and signSel1) = '1'
-                else  quot10 when (signSel0 and not signSel1) = '1'
-                else  quot01 when (not signSel0 and signSel1) = '1'
-                else  quot00 when (not signSel0 and not signSel1) = '1';
+            arg0 <= input.args(0);
+            arg1 <= input.args(1);
 
-           divRem <= rem11 when (signSel0 and signSel1) = '1'
-                else rem10 when (signSel0 and not signSel1) = '1'
-                else rem01 when (not signSel0 and signSel1) = '1'
-                else rem00 when (not signSel0 and not signSel1) = '1';
+            sg0 <= input.args(0)(31) and bool2std(input.st.operation.arith = opMulHS);
+            sg1 <= input.args(1)(31) and bool2std(input.st.operation.arith = opMulHS);
 
-        sendingDivIssued <= preInput.full and usesDivider(preInput); -- Speculative because it doesn't take into account kill signals?
-        sendingDivRR <= prevSending and usesDivider(input);
-
-        MAIN: process (clk)
-        begin
-            if rising_edge(clk) then
-                -- stage E0
-                isLow0 <= bool2std(input.st.operation.arith = opMul);
-
-                arg0 <= input.args(0);
-                arg1 <= input.args(1);
-
-                sgA <= input.args(0)(31) and bool2std(input.st.operation.arith = opMulHS);
-                sgB <= input.args(1)(31) and bool2std(input.st.operation.arith = opMulHS);
-
-                if sendingDivRR = '1' then
-                    signSel0 <= input.args(0)(31) and not bool2std(input.st.operation.arith = opDivU or input.st.operation.arith = opRemU);
-                    signSel1 <= input.args(1)(31) and not bool2std(input.st.operation.arith = opDivU or input.st.operation.arith = opRemU);
-                end if;
-
-
-                -- stage E1
-                if divReady = '1' then
-      
---                   if (signSel0 and signSel1) = '1' then
---                       divRes <= quot11; 
---                   elsif (signSel0 and not signSel1) = '1' then
---                       divRes <= quot10; 
---                   elsif (not signSel0 and signSel1) = '1' then
---                       divRes <= quot01; 
---                   else
---                       divRes <= quot00; 
---                   end if;
-                   
-                   divRes <= divQuot;
-                elsif remReady = '1' then
-                              
---                   if (signSel0 and signSel1) = '1' then
---                       divRes <= rem11; 
---                   elsif (signSel0 and not signSel1) = '1' then
---                       divRes <= rem10; 
---                   elsif (not signSel0 and signSel1) = '1' then
---                       divRes <= rem01; 
---                   else
---                       divRes <= rem00; 
---                   end if;
-                   
-                   divRes <= divRem;
-                end if;
-
-                isLow1 <= isLow0;
-                resLong1 <= work.Arith.multiplyLong(arg0, arg1, sgA, sgB);
-
-
-                -- stage E2
-                if divResultSent2 = '1' then
-                    mulResult <= divRes;
-                elsif isLow1 /= '1' then
-                    mulResult <= resLong1(63 downto 32);
-                else
-                    mulResult <= resLong1(31 downto 0);
-                end if;
+            -- stage E1
+            if divReady = '1' then
+               divRes <= divQuot;
+            elsif remReady = '1' then
+               divRes <= divRem;
             end if;
-        end process;
+
+            isLowE1 <= isLowE0;
+            resLongE1 <= work.Arith.multiplyLong(arg0, arg1, sg0, sg1);
+
+
+            -- stage E2
+            if divResultSent2 = '1' then
+                mulResult <= divRes;
+            elsif isLowE1 /= '1' then
+                mulResult <= resLongE1(63 downto 32);
+            else
+                mulResult <= resLongE1(31 downto 0);
+            end if;
+        end if;
+    end process;
+
+
+    divUnlock <= not (divAllowed and allowIssueI1) and not sendingDivIssued and not divRR and not divFull;
+
+
+    DIVISION: block
+        signal usingDiv, usingRem, trialled, kill: std_logic := '0';
+        signal divTime: SmallNumber := sn(0);
+        
+        signal new00, new10, new01, new11, isUnsigned, signSel0, signSel1: std_logic := '0';
+        signal divisorS, sum00, sum10, sum01, sum11, diff00, diff10, diff01, diff11: Dword := (others => '0');
+        signal result00, result10, result01, result11: Word := (others => '0');
+        signal quot00, quot10, quot01, quot11, rem00, rem10, rem01, rem11: Word := (others => '0');
+    begin
+        kill <= (trialled and events.execEvent) or events.lateEvent; -- move to division
 
 
 
 
-        divResultSending <= divFull and bool2std(slv2u(divTime) = 32) and not kill; -- TMP value
         divPrepareSend <= divFull and bool2std(slv2u(divTime) = 30); -- TMP value
-
-        divUnlock <= not (divAllowed and allowIssueI1) and not sendingDivIssued and not divRR and not divFull;
+        divResultSending <= divFull and bool2std(slv2u(divTime) = 32) and not kill; -- TMP value
 
 
         DIVIDER_STATE: process (clk)
@@ -175,7 +149,7 @@ begin
 
                 if divResultSending = '1' or kill = '1' then
                     divFull <= '0';
-                        divSlot.dbInfo <= DEFAULT_DEBUG_INFO;
+                    divSlot.dbInfo <= DEFAULT_DEBUG_INFO;
 
                     usingDiv <= '0';
                     usingRem <= '0';
@@ -188,7 +162,6 @@ begin
                     usingDiv <= bool2std(input.st.operation.arith = opDivU or input.st.operation.arith = opDivS);
                     usingRem <= bool2std(input.st.operation.arith = opRemU or input.st.operation.arith = opRemS);
 
-                    isUnsigned <= bool2std(input.st.operation.arith = opDivU or input.st.operation.arith = opRemU);
                 else
                     divTime <= addInt(divTime, 1);
                 end if;
@@ -196,7 +169,18 @@ begin
             end if;
         end process;
 
-        
+
+
+       divQuot <= quot11 when (signSel0 and signSel1) = '1'
+            else  quot10 when (signSel0 and not signSel1) = '1'
+            else  quot01 when (not signSel0 and signSel1) = '1'
+            else  quot00 when (not signSel0 and not signSel1) = '1';
+
+       divRem <= rem11 when (signSel0 and signSel1) = '1'
+            else rem10 when (signSel0 and not signSel1) = '1'
+            else rem01 when (not signSel0 and signSel1) = '1'
+            else rem00 when (not signSel0 and not signSel1) = '1';
+
         diff00 <=    add(sum00, divisorS) when --isNonzero(divTime) /= '1'
                                                false
                 else sub(sum00, divisorS);
@@ -234,6 +218,10 @@ begin
                     quot11 <= result11;
                     rem11  <= sum11(31 downto 0);
                 elsif sendingDivRR = '1' then
+                    isUnsigned <= bool2std(input.st.operation.arith = opDivU or input.st.operation.arith = opRemU);
+                    signSel0 <= input.args(0)(31) and not bool2std(input.st.operation.arith = opDivU or input.st.operation.arith = opRemU);
+                    signSel1 <= input.args(1)(31) and not bool2std(input.st.operation.arith = opDivU or input.st.operation.arith = opRemU);
+                
                     result00 <= (others => '0');
                     sum00 <= zeroExtend(input.args(0), 64);
 
