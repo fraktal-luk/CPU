f8dc9d252f0c19bab38fe535b2785213dafffa32
fraktal-luk <fraktal3725@gmail.com>
2023-12-21 12:26:45 +0100

    Generation of ins word kinda works

--- project_1.srcs/sim_1/new/InsDefs.sv
+++ project_1.srcs/sim_1/new/InsDefs.sv
@@ -80,6 +80,125 @@ type ProcMnemonic is ( -- one word instruction names, distinguishing different a
             floatToInt, intToFloat
     } InstructionFormat;
 
+        typedef enum {
+            P_none,
+            P_ja,
+            P_jl,
+            P_jz,
+            P_jnz,
+            P_intAlu,
+            P_floatOp,
+            P_intMem,
+            P_floatMem,
+            P_intAluImm,
+            P_addI,
+            P_addH, 
+            
+            P_intLoadW16,
+            P_intStoreW16,
+            P_floatLoadW16,
+            P_floatStoreW16,
+            
+            P_sysMem,
+            
+            P_sysControl
+        } Primary;
+    
+        typedef enum {
+            S_none,
+            
+            // P_intAlu          
+            S_intLogic, S_intArith, S_jumpReg, S_intMul,
+             
+            // P_intAluImm
+            S_intShiftLogical, S_intShiftArith, S_intRotate,
+             
+            // P_floatOp
+            S_floatMove,
+             
+            // P_intMem
+            S_intLoadW, S_intStoreW,
+             
+            // P_floatMem
+            S_floatLoadW, S_floatStoreW,
+             
+            // P_sysMem
+            S_sysLoad, S_sysStore,
+             
+            // P_sysControl                 
+            S_sysUndef,
+            S_sysError,
+            S_sysCall,
+            S_sysRetE,
+            S_sysRetI,
+            S_sysHalt,
+            S_sysSync,
+            S_sysReplay,
+            S_sysSend                 
+         } Secondary;
+
+        typedef enum {
+            T_none, 
+
+            T_intAnd, T_intOr, T_intXor,
+        
+            T_intAdd, T_intSub,  
+        
+            T_intMul, T_intMulHU, T_intMulHS,
+            T_intDivU, T_intDivS,
+            T_intRemU, T_intRemS,
+        
+            T_floatMove,
+          
+            T_jumpRegZ, T_jumpRegNZ
+        } Ternary;
+
+    //endclass;
+    
+        typedef enum {
+            O_undef,
+            O_call,
+            O_sync,
+            O_retE,
+            O_retI,
+            O_replay,
+            O_halt,
+            O_send,
+            
+            O_jump,
+            
+            O_intAnd, O_intOr, O_intXor,
+            O_intAdd, O_intSub,
+            O_intAddH,
+            O_intMul, O_intMulHU, O_intMulHS,
+            O_intDivU, O_intDivS,
+            O_intRemU, O_intRemS,
+            
+            O_intShiftLogical, O_intShiftArith, O_intRotate,
+            
+            O_floatMove,
+            
+            O_intLoadW, O_intLoadD,
+            
+            O_intStoreW, O_intStoreD,
+            
+            O_floatLoadW, O_floatStoreW,
+            
+            O_sysLoad, O_sysStore
+            
+        } Operation;
+
+
+
+    typedef struct {
+        
+        Primary p;
+        Secondary s;
+        Ternary t;
+        Operation o;
+    } InstructionDef;
+
+
         typedef InstructionFormat FormatMap[string];
         
         const FormatMap formatMap = '{
@@ -221,6 +340,9 @@ type ProcMnemonic is ( -- one word instruction names, distinguishing different a
     };
     
     
+    typedef string string4[4];
+    typedef Word Word4[4];
+    
     function automatic void getParsing(input string s);
         InstructionFormat fmt = getFormat(s);
         $display("%s: %s ->  '%s', '%s', '%s'", s, fmt.name(), parsingMap[fmt][0], parsingMap[fmt][1], parsingMap[fmt][2]);
@@ -229,128 +351,28 @@ type ProcMnemonic is ( -- one word instruction names, distinguishing different a
 
     function automatic void TMP_showArgs(input string parts[]);
         InstructionFormat fmt = getFormat(parts[0]);
+        InstructionDef def = getDef(parts[0]);
         
         string out[] = orderArgs(parts[1:3], parsingMap[fmt]);
+        Word4 vals;
+        Word res;
         
         $display("%s %s %s %s", out[0], out[1], out[2], out[3]);
+        
+        $display("%d", checkArgs(out[0:3], parsingMap[fmt]));// $display("Args error!");
+        
+        vals = parseArgs(out[0:3]);
+        
+        $display("%p", vals);
+        
+        res = fillArgs(vals, parsingMap[fmt], 0);
+        $write("%b ", res);
+        
+        res = fillOp(res, def);
+        $display("%b", res);
     endfunction;
 
    // class Opcodes;
-        typedef enum {
-            P_none,
-            P_ja,
-            P_jl,
-            P_jz,
-            P_jnz,
-            P_intAlu,
-            P_floatOp,
-            P_intMem,
-            P_floatMem,
-            P_intAluImm,
-            P_addI,
-            P_addH, 
-            
-            P_intLoadW16,
-            P_intStoreW16,
-            P_floatLoadW16,
-            P_floatStoreW16,
-            
-            P_sysMem,
-            
-            P_sysControl
-        } Primary;
-    
-        typedef enum {
-            S_none,
-            
-            // P_intAlu          
-            S_intLogic, S_intArith, S_jumpReg, S_intMul,
-             
-            // P_intAluImm
-            S_intShiftLogical, S_intShiftArith, S_intRotate,
-             
-            // P_floatOp
-            S_floatMove,
-             
-            // P_intMem
-            S_intLoadW, S_intStoreW,
-             
-            // P_floatMem
-            S_floatLoadW, S_floatStoreW,
-             
-            // P_sysMem
-            S_sysLoad, S_sysStore,
-             
-            // P_sysControl                 
-            S_sysUndef,
-            S_sysError,
-            S_sysCall,
-            S_sysRetE,
-            S_sysRetI,
-            S_sysHalt,
-            S_sysSync,
-            S_sysReplay,
-            S_sysSend                 
-         } Secondary;
-
-        typedef enum {
-            T_none, 
-
-            T_intAnd, T_intOr, T_intXor,
-        
-            T_intAdd, T_intSub,  
-        
-            T_intMul, T_intMulHU, T_intMulHS,
-            T_intDivU, T_intDivS,
-            T_intRemU, T_intRemS,
-        
-            T_floatMove,
-          
-            T_jumpRegZ, T_jumpRegNZ
-        } Ternary;
-
-    //endclass;
-    
-        typedef enum {
-            O_undef,
-            O_call,
-            O_sync,
-            O_retE,
-            O_retI,
-            O_replay,
-            O_halt,
-            O_send,
-            
-            O_jump,
-            
-            O_intAnd, O_intOr, O_intXor,
-            O_intAdd, O_intSub,
-            O_intAddH,
-            O_intMul, O_intMulHU, O_intMulHS,
-            O_intDivU, O_intDivS,
-            O_intRemU, O_intRemS,
-            
-            O_intShiftLogical, O_intShiftArith, O_intRotate,
-            
-            O_floatMove,
-            
-            O_intLoadW, O_intLoadD,
-            
-            O_intStoreW, O_intStoreD,
-            
-            O_floatLoadW, O_floatStoreW,
-            
-            O_sysLoad, O_sysStore
-            
-        } Operation;
-
-    typedef struct {
-        
-        Primary p;
-        Secondary s;
-        Ternary t;
-        Operation o;
-    } InstructionDef;
 
 
         typedef InstructionDef DefMap[string];
@@ -434,8 +456,7 @@ type ProcMnemonic is ( -- one word instruction names, distinguishing different a
         end  
     endfunction
 
-    typedef string string4[4];
-    
+
     function automatic string4 orderArgs(input string args[], input string3 parsingMap);
         // d 0 1 2   a0 a1 a2 a3
         string4 out;
@@ -459,6 +480,204 @@ type ProcMnemonic is ( -- one word instruction names, distinguishing different a
 
     function automatic bit checkArgs(input string4 args, input string3 parsingMap);
         
+//    asmForm: string(1 to 4);
+//    decoding: string(1 to 5);
+//    typeSpec: string(1 to 5);
+        string typeSpec = parsingMap[2];    
+        string decoding = parsingMap[1];
+        
+        case (typeSpec[0])
+            "i": if (args[0][0] != "r" && decoding[0] != "0") return 2;
+            "f": if (args[0][0] != "f" && decoding[0] != "0") return 3;
+            default: if (args[0][0] != " ") return 4
+                        ;
+        endcase
+
+        for (int i = 0; i <= 3; i++) begin
+            case (typeSpec[i+1])
+                "i": if (args[i][0] != "r" && decoding[i+1] != "0") return 5 + 10*i;
+                "f": if (args[i][0] != "f" && decoding[i+1] != "0") return 6 + 10*i;
+                "c": ;
+                default: if (args[i][0] != " ") return 7 + 10*i;
+            endcase
+        end
+        
+        return 1;
     endfunction
+
+
+    function automatic Word4 parseArgs(input string4 args);
+        Word4 res;
+        integer value = 'x;
+
+        foreach(args[i]) begin
+            if (args[i].len() == 0) begin
+               res[i] = 'x;
+               continue;
+            end
+        
+            case (args[i][0])
+                "$", "@": value = 'x;
+                "f":      value = args[i].substr(1, args[i].len()-1).atoi();
+                "r":      value = args[i].substr(1, args[i].len()-1).atoi();
+                "-":      value = args[i].substr(1, args[i].len()-1).atoi();
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9": 
+                          value = args[i].atoi();
+                default: $fatal("Wrong arg");
+            endcase
+            
+            res[i] = value;
+        end
+        
+        return res;
+    endfunction
+
+
+    
+    function automatic Word fillField(input Word w, input logic[7:0] field, input Word value);
+        Word res = w;
+        case (field)
+            "a": res[25:21] = value[4:0];
+            "b": res[20:16] = value[4:0];
+            "c": res[9:5] = value[4:0];
+            "d": res[4:0] = value[4:0];
+            "X": res[4:0] = value[4:0];
+            "H": res[15:0] = value[15:0];
+            "J": res[20:0] = value[20:0];
+            "L": res[25:0] = value[25:0];
+            " ", "0": ;
+            default: $fatal("Invalid field: %s", field);
+        endcase
+        
+        return res;
+    endfunction
+
+
+//-- Insert args into word
+//function TMP_fillArgs(args: IntArray; desc: NEW_FormatDescription; undefOffset: boolean) return Word is
+    function automatic Word fillArgs(input Word4 args, input string3 parsingMap, input bit unknownOffset);
+        string decoding = parsingMap[1];
+        Word res = '0;
+//    variable res: Word := (others => '0');
+//begin
+//    case desc.decoding(1) is
+//        when 'a' =>
+//            res(25 downto 21) := i2slv(args(0), 5);
+//        when 'b' =>
+//            res(20 downto 16) := i2slv(args(0), 5);
+//        when 'c' =>
+//            res(9 downto 5) := i2slv(args(0), 5);
+//        when 'd' =>
+//            res(4 downto 0) := i2slv(args(0), 5);                                                
+//        when others =>
+//    end case;
+       res = fillField(res, decoding[0], args[0]);
+       
+       res = fillField(res, decoding[2], args[1]);
+       res = fillField(res, decoding[3], args[2]);
+       res = fillField(res, decoding[4], args[3]);
+        
+//    case desc.decoding(3) is -- Skip the comma!
+//        when 'a' =>
+//            res(25 downto 21) := i2slv(args(1), 5);
+//        when 'b' =>
+//            res(20 downto 16) := i2slv(args(1), 5);
+//        when 'c' =>
+//            res(9 downto 5) := i2slv(args(1), 5);
+//        when 'd' =>
+//            res(4 downto 0) := i2slv(args(1), 5);
+//        when 'X' =>
+//            res(9 downto 0) := i2slv(args(1), 10);
+//        when 'H' =>
+//            res(15 downto 0) := i2slv(args(1), 16);
+//        when 'J' =>
+//            res(20 downto 0) := i2slv(args(1), 21);
+//        when 'L' =>
+//            res(25 downto 0) := i2slv(args(1), 26);                                                            
+//        when others =>
+//    end case;
+    
+//    case desc.decoding(4) is
+//        when 'a' =>
+//            res(25 downto 21) := i2slv(args(2), 5);
+//        when 'b' =>
+//            res(20 downto 16) := i2slv(args(2), 5);
+//        when 'c' =>
+//            res(9 downto 5) := i2slv(args(2), 5);
+//        when 'd' =>
+//            res(4 downto 0) := i2slv(args(2), 5);
+//        when 'X' =>     
+//            res(9 downto 0) := i2slv(args(2), 10);
+//            if undefOffset then
+//                res(9 downto 0) := (others => 'U');
+//            end if;
+//        when 'H' =>
+//            res(15 downto 0) := i2slv(args(2), 16);
+//            if undefOffset then
+//                res(15 downto 0) := (others => 'U');
+//            end if;
+//        when 'J' =>
+//            res(20 downto 0) := i2slv(args(2), 21);
+//            if undefOffset then
+//                res(20 downto 0) := (others => 'U');
+//            end if;
+//        when 'L' =>
+//            res(25 downto 0) := i2slv(args(2), 26);
+//            if undefOffset then
+//                res(25 downto 0) := (others => 'U');
+//            end if;                                                            
+//        when others =>
+//    end case;
+    
+//    case desc.decoding(5) is
+//        when 'a' =>
+//            res(25 downto 21) := i2slv(args(3), 5);
+//        when 'b' =>
+//            res(20 downto 16) := i2slv(args(3), 5);
+//        when 'c' =>
+//            res(9 downto 5) := i2slv(args(3), 5);
+//        when 'd' =>
+//            res(4 downto 0) := i2slv(args(3), 5);
+//        when 'X' =>
+//            res(9 downto 0) := i2slv(args(3), 10);
+//        when 'H' =>
+//            res(15 downto 0) := i2slv(args(3), 16);
+//        when 'J' =>
+//            res(20 downto 0) := i2slv(args(3), 21);
+//        when 'L' =>
+//            res(25 downto 0) := i2slv(args(3), 26);                                                            
+//        when others =>
+//    end case;   
+
+//    return res;
+//end function;
+        return res;
+    endfunction
+    
+//function TMP_fillOp(w: Word; insDef: InstructionDefinition) return Word is
+    function automatic Word fillOp(input Word w, input InstructionDef def);
+        Word res = w;
+        
+        res[31:26] = def.p.num();
+        if (def.s != S_none) res[15:10] = def.s.num();
+        if (def.t != T_none) res[4:0] = def.t.num();
+//    variable res: Word := w;
+//begin
+//    res(31 downto 26) := i2slv(insDef.i, 6);
+    
+//    if insDef.j = -1 then return res; end if;
+    
+//    res(15 downto 10) := i2slv(insDef.j, 6);
+
+//    if insDef.k = -1 then return res; end if;
+
+//    res(4 downto 0) := i2slv(insDef.k, 5);
+    
+//    return res;
+//end function;
+        return res;
+    endfunction;
+
+
 
 endpackage
